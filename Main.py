4934afc2d60945293caff718457b26e3ecd0d191
RymBdeb <6353269@bdeb.qc.ca>
2025-06-27 11:53:25 -0400

    version 1

--- Main.py
+++ Main.py
@@ -1,19 +1,20 @@
+import unittest
+
+# Fonction à tester
 def addition(a, b):
     return a + b
 
+# Classe de test
+class TestAddition(unittest.TestCase):
+    def test_addition(self):
+        # Assertions pour vérifier si le résultat est conforme à ce qui est attendu
+        self.assertEqual(addition(2, 3), 5)
+        self.assertEqual(addition(7, 8), 15)
+        self.assertEqual(addition(9, 3), 12)
 
-# TDD : développement dirigé par les tests
-# Écrivez une méthode de test pour la fonction
-def test_addition():
-    # Appeler la fonction à tester
-    result = addition(3, 5)
-    # Assertion pour vérifier si le résultat est celui attendu
-    assert result == 8, f"Le résultat attendu est 8, mais le résultat obtenu est {result}"
+# Définition d'un objet test
+test_addition = unittest.TestLoader().loadTestsFromTestCase(TestAddition)  # On charge les tests depuis la classe
 
-    result = addition(3, 9)
-    assert result == 12, f"Le résultat attendu est 12, mais le résultat obtenu est {result}"
+# Exécution du test
+unittest.TextTestRunner().run(test_addition)
 
-
-# Créez une suite de tests et exécutez-les
-test_addition()
-print("Test terminé ...")
